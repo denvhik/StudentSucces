2fb8d06965d46bedcd2b3b5a529944c89ff0aa41
Denys D.H. Hovdysh <denys.hovdysh@DEV.COM.UA>
2024-03-20 15:13:04 +0200

    RefactoringTriggers

--- Triggers.sql
+++ Triggers.sql
@@ -1,15 +1,16 @@
-CREATE TRIGGER CheckBorrowedBooksAmount
-ON StudentBook
+
+CREATE OR ALTER TRIGGER [CheckBorrowedBooksAmount]
+ON [StudentBook]
 AFTER INSERT, UPDATE
 AS
 BEGIN
     IF EXISTS (
-        SELECT sb.StudentID
-        FROM inserted i
-        JOIN StudentBook sb ON i.BookID = sb.BookID
-        JOIN Book b ON sb.BookID = b.BookID
-        GROUP BY sb.StudentID
-        HAVING SUM(b.Price) > 100
+        SELECT [sb].[StudentID]
+        FROM [inserted] [i]
+        JOIN [StudentBook] [sb] ON i.[BookID] = [sb].[BookID]
+        JOIN [Book] [b] ON [sb].[BookID] = [b].[BookID]
+        GROUP BY [sb].[StudentID]
+        HAVING SUM([b].[Price]) > 100
     )
     BEGIN
         RAISERROR ('Total borrowed books price exceeds 100 UAH for one or more students.', 16, 1);
@@ -18,16 +19,16 @@ BEGIN
 END;
 
 GO
-CREATE TRIGGER CheckRoomCapacity
-ON StudentsDormitory
+CREATE OR ALTER TRIGGER [CheckRoomCapacity]
+ON [StudentsDormitory]
 AFTER INSERT, UPDATE
 AS
 BEGIN
     IF EXISTS (
-        SELECT s.DormitoryID
-        FROM inserted i
-        JOIN StudentsDormitory s ON i.DormitoryID = s.DormitoryID
-        GROUP BY s.DormitoryID
+        SELECT [s].[DormitoryID]
+        FROM [inserted] [i]
+        JOIN [StudentsDormitory] [s] ON [i].[DormitoryID] = [s].[DormitoryID]
+        GROUP BY [s].[DormitoryID]
         HAVING COUNT(*) > 3
     )
     BEGIN
